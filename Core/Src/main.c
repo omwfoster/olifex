05d9d61544be91d248d886444a087c8c5535c057
oli foster <omwfoster@gmail.com>
2021-11-09 14:00:42 +0000

    Revert "safe build - no hardfault"
    This reverts commit f166a0b8ca34f8f506033c671261e5bb5818f639.

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -262,9 +262,8 @@ static void MX_DMA_Init(void)
 static void MX_GPIO_Init(void)
 {
 
-  /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOC_CLK_ENABLE();
-  __HAL_RCC_GPIOH_CLK_ENABLE();
+	/* GPIO Ports Clock Enable */
+	__HAL_RCC_GPIOD_CLK_ENABLE();
 
 }
 
